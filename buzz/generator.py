4f6b487f15cd1d6ee89e0e67acce18214d799328
Pavi Anaji <pavianaji@gmail.com>
2019-11-11 18:29:37 +0530

    Add Heroku yaml file

--- buzz/generator.py
+++ buzz/generator.py
@@ -15,7 +15,7 @@ def sample(l, n=1):
 
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
-    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs), sample(verbs), buzz_terms[1], " - Pavi (the CI CD Expert)"])
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs), sample(verbs), buzz_terms[1], " - Pavi (the wife of CI CD Expert)"])
     return phrase.title()
 
 
